e91313245d81f14b3fd307e7c3b79858e5413043
Ana Paula Hiraiwa <hhiraiwa1@hotmail.com>
2025-01-12 09:16:24 -0300

    ass container input

---
+++ main.js
@@ -0,0 +1,5 @@
+const botaoEnviar = document.getElementById("#enviar");
+const campoInput = document.querySelector("input");
+const listaValores = document.querySelector("lista");
+
+botaoEnviar.addEventListener("click");
